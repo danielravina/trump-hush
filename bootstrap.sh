d2662f7c2f1dc719a1d8fd35a5086f62055d2dcb
DanielRavina <danielravina@gmail.com>
2016-06-12 16:01:26 -0700

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
-if [ $PGPASSWORD == "" ]; then
-  echo "PGPASSWORD is missing";
-  exit
-done
-
 echo "updating / upgrading box"
-apt-get upgrade && apt-get update -y 1> /dev/null
 
-packagelist= (
+sudo apt-get upgrade -y 2>&1 >/dev/null
+sudo apt-get update -y 2>&1 >/dev/null
+
+packagelist=(
   build-essential
   libeigen3-dev
   git
@@ -24,6 +21,7 @@ packagelist= (
   libhdf5-serial-dev
   libhdf5-7
   python-pip
+  python-dev
   postgresql
   postgresql-contrib
   libav-tools
@@ -33,7 +31,23 @@ packagelist= (
 
 echo "Installing required packages"
 
-apt-get install ${packagelist[@]} -y 1> /dev/null
+sudo apt-get install ${packagelist[@]} -y 2>&1 >/dev/null
+
+# Redis
+echo "Installing redis"
+cd ~
+wget http://download.redis.io/releases/redis-stable.tar.gz 2>&1 >/dev/null
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make 2>&1 >/dev/null
+make test 2>&1 >/dev/null
+sudo make install 2>&1 >/dev/null
+cd utils
+sudo ./install_server.sh 2>&1 >/dev/null
+sudo service redis_6379 start
+cd ../../;
+rm -R redis-stable
+rm redis-stable.tar.gz
 
 echo "Installing Yaafe"
 cd ~
@@ -42,32 +56,25 @@ cd yaafe
 mkdir build
 cd build
 ccmake ..
-make && make install
+make && sudo make install
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/" >> ~/.bashrc; source ~/.bashrc
 
 echo "Installing youtube-dl"
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-
-# Redis
-echo "Installing redis"
-wget http://download.redis.io/releases/redis-stable.tar.gz
-tar xzf redis-stable.tar.gz
-cd redis-stable
-make test
-make install
-cd utils
-./install_server.sh
-service redis_6379 start
-
-
+sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl 2>&1 >/dev/null
 
 echo "Install application code"
-mkdir -p /home/app
-cd /home/app
-git clone https://github.com/danielravina/trump-learn
-cd trump-learn
-pip install -r requirements.txt
+sudo mkdir -p /home/app
+sudo useradd app
 
+cd ~
+pip install --upgrade pip
+sudo pip install virtualenv
+virtualenv .
+source bin/activate
+git clone https://github.com/danielravina/trump-learn 2>&1 >/dev/null
+cd trump-learn
+pip install numpy cython
+pip install -r 'requirements.txt'
 
 # Set psql (MANUALLY):
 # 1. useradd app
